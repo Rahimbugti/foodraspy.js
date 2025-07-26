76afaf79940d8266fd8c9304b68c73312ff0510d
Rahimbugti <162613347+Rahimbugti@users.noreply.github.com>
2025-07-26 15:46:03 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Login from './components/Login'
+import './App.css'
+// import'./index.css'
+//  import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import Movies from './MovieZone/Movies'
+import Food from './FoodRaspy/Food'
+
+
+const App = () => {
+  return (
+    <>
+    <Food/> 
+    </>
+  )
+}
+
+export default App
